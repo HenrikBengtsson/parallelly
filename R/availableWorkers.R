57df835229afb2a319b98e1e85174d616af9ed57
Henrik Bengtsson <hb@aroma-project.org>
2020-10-05 23:19:10 -0700

    HELP: availableWorkers() is now in the help index

--- R/availableWorkers.R
+++ R/availableWorkers.R
@@ -53,7 +53,6 @@
 #'
 #' @importFrom utils file_test
 #' @export
-#' @keywords internal
 availableWorkers <- function(methods = getOption2("future.availableWorkers.methods", c("mc.cores", "_R_CHECK_LIMIT_CORES_", "PBS", "SGE", "Slurm", "LSF", "custom", "system", "fallback")), na.rm = TRUE, default = "localhost", which = c("auto", "min", "max", "all")) {
   ## Local functions
   getenv <- function(name) {
