1b11e7eb822a2658056cd79ef79c33ffc33b7b11
Henrik Bengtsson <hb@aroma-project.org>
2021-07-15 14:35:30 +0200

    Force setup_strategy = 'sequential' also when 'tcltk' is loaded

--- R/parallelly_disable_parallel_setup_if_needed.R
+++ R/parallelly_disable_parallel_setup_if_needed.R
@@ -5,19 +5,17 @@
 ## This bug (https://bugs.r-project.org/bugzilla/show_bug.cgi?id=18119)
 ## has been fixed in R-devel r80472 (2021-06-10) and in R-4.1-branch in
 ## r80532 (2021-06-19).
-parallelly_disable_parallel_setup_if_needed <- function() {
+##
+## UPDATE 2021-07-15: It turns out that this bug also affects macOS if
+## the 'tcltk' package is loaded, cf.
+## https://github.com/rstudio/rstudio/issues/6692#issuecomment-880647623
+parallelly_disable_parallel_setup_if_needed <- function(liberal=TRUE) {
   ## Always respect users settings
   if (!is.null(getOption("parallelly.makeNodePSOCK.setup_strategy"))) return()
 
   ## Nothing to do because of old version of R?
   rver <- getRversion()
   if (rver < "4.0.0") return()
-
-  ## Nothing to do because we're not running in RStudio Console?
-  if (Sys.getenv("RSTUDIO") != "1") return()
-  if (nzchar(Sys.getenv("RSTUDIO_TERM"))) return()
-
-  ## Can we Nothing to do for 'parallel', i.e. its internal option 'setup_strategy'
 
   ## We only need to disable 'parallel' setup for certain R versions
   rev <- as.integer(R.version[["svn rev"]])
@@ -36,6 +34,18 @@ parallelly_disable_parallel_setup_if_needed <- function() {
     return()
   }
 
+  if (liberal) {
+    ## (a) Running RStudio Console?
+    automagic <- Sys.getenv("RSTUDIO") == "1" &&
+                 !nzchar(Sys.getenv("RSTUDIO_TERM"))
+                 
+    ## (b) Or has 'tcltk' been loaded?
+    automagic <- automagic || ("tcltk" %in% loadedNamespaces())
+  
+    ## Nothing to do?
+    if (!automatic) return()
+  }
+  
   ## Force 'parallelly' to use the "sequential" setup strategy
   options(parallelly.makeNodePSOCK.setup_strategy = "sequential")
 
