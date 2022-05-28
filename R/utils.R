cd6e04d46b625d92bbc2d09c498f1d81d755a9fb
Henrik Bengtsson <hb@aroma-project.org>
2022-05-28 10:41:07 -0700

    CLEANUP: Moved stopf(), warnf(), and msgf() to separate file (easier to reuse in other packages this way)

--- R/utils.R
+++ R/utils.R
@@ -15,43 +15,6 @@ assert_no_positional_args_but_first <- function(call = sys.call(sys.parent())) {
   }
 }
 
-stopf <- function(fmt, ..., call. = TRUE, domain = NULL) {  #nolint
-  msg <- sprintf(fmt, ...)
-  msg <- .makeMessage(msg, domain = domain)
-  if (is.call(call.)) {
-    call <- call.
-  } else if (isTRUE(call)) {
-    call <- sys.call(which = -1L)
-  } else {
-    call <- NULL
-  }
-  cond <- simpleError(msg, call = call)
-  stop(cond)
-}
-
-warnf <- function(fmt, ..., call. = TRUE, immediate. = FALSE, domain = NULL) {  #nolint
-  msg <- sprintf(fmt, ...)
-  ## Cannot tweak 'call' when immediate. = TRUE
-  if (isTRUE(immediate.)) {
-    warning(msg, call. = call., immediate. = immediate., domain = domain)
-  } else {
-    msg <- .makeMessage(msg, domain = domain)
-    if (is.call(call.)) {
-      call <- call.
-    } else if (isTRUE(call)) {
-      call <- sys.call(which = -1L)
-    } else {
-      call <- NULL
-    }
-    cond <- simpleWarning(msg, call = call)
-    warning(cond)
-  }
-}
-
-msgf <- function(fmt, ..., appendLF = FALSE, domain = NULL) {  #nolint
-  message(sprintf(fmt, ...), appendLF = appendLF, domain = domain)
-}
-
 stop_if_not <- function(...) {
   res <- list(...)
   for (ii in 1L:length(res)) {
