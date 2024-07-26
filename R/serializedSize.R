1ef9d97e689b70d2af8245c0900b63e840bb80f0
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2024-07-26 17:30:47 +0200

    Tweak serializedSize()

--- R/serializedSize.R
+++ R/serializedSize.R
@@ -3,16 +3,17 @@
 #' This function goes through all the motions of serializing an object, but
 #' does nothing with the bytes other than to tally the total length.
 #'
-#' @param robj An R object.
+#' @param obj An R object.
 #'
 #' @return (integer or double) Number of bytes needed to serialize this object.
 #'
 #' @examples
+#' object.size(mtcars)
 #' serializedSize(mtcars)
 #'
 #' @author Mike FC
 #' 
 #' @export
-serializedSize <- function(robj) {
-  .Call(C_R_calc_serialized_size, robj)
+serializedSize <- function(obj) {
+  .Call(C_R_calc_serialized_size, obj)
 }
